a8f862f087a38bc6c7216bc3187faa7bbf84dade
rios-pedro <pedro.henrique9760@icloud.com>
2026-04-30 00:28:52 -0300

    redirect root to swagger

---
+++ src/main/java/pedrorios/contactapi/controller/SwaggerRedirectController.java
@@ -0,0 +1,13 @@
+package pedrorios.contactapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerRedirectController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger";
+    }
+}
